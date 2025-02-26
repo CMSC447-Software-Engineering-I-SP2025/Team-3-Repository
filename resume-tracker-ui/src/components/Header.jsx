c47c120e2d999de3dc1ae8b632d9f6daefbeded2
aangar <zekrom514@gmail.com>
2025-02-26 17:22:08 -0500

    CPB-10: fix?

---
+++ resume-tracker-ui/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Grid2 from '@mui/material/Grid2'
+
+const Header = () =>
+  <Grid2 size={12}>
+    <Grid2 container >
+      <Grid2 size={3} >meep</Grid2>
+      <Grid2 size={3} >meep</Grid2>
+      <Grid2 size={3} >meep</Grid2>
+    </Grid2>
+  </Grid2>
+
+export default Header
---
+++ resume-tracker-ui/src/theme/index.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    background: '#d7d7d9'
+  }
+})
+
+export default theme

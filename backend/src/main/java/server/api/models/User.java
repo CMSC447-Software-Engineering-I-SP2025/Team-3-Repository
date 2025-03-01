d11b71e925f7823e618f92558192636a79cb9e94
icedoutbart <carterl1@umbc.edu>
2025-02-28 19:00:44 -0500

    CPB-14: Added User Data Model

---
+++ backend/src/main/java/server/api/models/User.java
@@ -0,0 +1,22 @@
+package server.api.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private ObjectId id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<String> skills;
+}

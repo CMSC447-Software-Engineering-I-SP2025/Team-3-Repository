95cf6dfbeed6e17348623700336b1891f60d5499
aangar <zekrom514@gmail.com>
2025-03-02 15:16:52 -0500

    CPB-57: fix - adding nextjs mui integration

--- application-tracker-ui/src/app/layout.js
+++ application-tracker-ui/src/app/layout.js
@@ -1,8 +1,8 @@
 import Header from "@/components/layout/Header";
 import TopBar from "@/components/layout/TopBar";
 import theme from "@/theme";
-import { AccountCircle } from "@mui/icons-material";
 import { Box, ThemeProvider } from "@mui/material";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 
 export const metadata = {
   title: "Application Tracker UI",
@@ -12,23 +12,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <ThemeProvider theme={theme}>
-        <body
-          style={{
-            margin: 0,
-            minHeight: '100vh',
-            display: 'flex'
-          }}
-        >
-          <Box sx={{ flex: 1, flexGrow: 1 }} >
-            <Header/>
-            <Box sx={{ marginLeft: '60px', backgroundColor: 'background', height: '100%' }}>
-              <TopBar/>
-              { children }
+      <body
+        style={{
+          margin: 0,
+          minHeight: '100vh',
+          display: 'flex'
+        }}
+      >
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <Box sx={{ flex: 1, flexGrow: 1 }} >
+              <Header/>
+              <Box sx={{ marginLeft: '60px', backgroundColor: 'background', height: '100%' }}>
+                <TopBar/>
+                { children }
+              </Box>
             </Box>
-          </Box>
-        </body>
-      </ThemeProvider>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }

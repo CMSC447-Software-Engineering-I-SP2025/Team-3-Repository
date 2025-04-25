40b43d6c408ad0728cfbcaed295ff6313a944389
icedoutbart <carterl1@umbc.edu>
2025-04-25 17:09:56 -0400

    Added jobs sample data and updated load-data script

--- sample-data/load-data.sh
+++ sample-data/load-data.sh
@@ -1,2 +1,3 @@
 mongoimport --db=application-tracker --collection=users --drop --jsonArray users.json
 mongoimport --db=application-tracker --collection=application --drop --jsonArray applications.json
+mongoimport --db=application-tracker --collection=job --drop --jsonArray jobs.json
